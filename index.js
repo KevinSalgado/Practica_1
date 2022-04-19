214e2bc2d307df51b87515911f1915903adeca65
Kevin_Salgado <sgkevinsg@gmail.com>
2022-04-19 18:06:00 -0500

    jeje

---
+++ index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require('./app/MissionCommander');
+const kevin = new MissionCommander("Kevin")
+const diogo = new MissionCommander("Diogo")
+const carlo = new MissionCommander("Carlo")
+console.log(carlo.name)
+console.log(kevin.name)
+console.log(diogo.name)
